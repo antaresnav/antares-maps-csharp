da81e9767b281388c8f9f755921c0899b53e1930
Gazdag Imre <gazdag@geox.hu>
2019-01-22 15:14:45 +0100

    Update 0.2.2

--- samples/UWPSample/UWPSample/MainPage.xaml.cs
+++ samples/UWPSample/UWPSample/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Windows.UI.ViewManagement;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -23,26 +28,73 @@ namespace UWPSample
             MapView.MinZoomLevel = 4;
         }
 
-        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private async void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var comboBox = sender as ComboBox;
 
             ComboBoxItem value = comboBox?.SelectedItem as ComboBoxItem;
             switch (value?.Name)
             {
+                case "Dsm":
+                    MapView.SetStyleUrl("https://terkep.geox.hu/rutin/rutin_dsm10_style_dev_all.json");
+                    break;
                 case "OsmBright":
                     MapView.SetStyleUrl("https://raw.githubusercontent.com/antaresnav/antares-maps-sharp/develop/styles/osm_bright.json");
                     break;
                 case "OsmBasic":
                     MapView.SetStyleUrl("https://raw.githubusercontent.com/antaresnav/antares-maps-sharp/develop/styles/osm_basic.json");
                     break;
+                case "Openmaptiles":
+                    MapView.SetStyleUrl("https://raw.githubusercontent.com/antaresnav/antares-maps-sharp/develop/styles/openmaptiles.json");
+                    break;
                 case "HereHybrid":
                     MapView.SetStyleUrl("https://raw.githubusercontent.com/antaresnav/antares-maps-sharp/develop/styles/here_hybrid.json");
                     break;
                 case "OfflineMap":
+                 /*   using (var stream = GetType().GetTypeInfo().Assembly.GetManifestResourceStream("UWPSample.basic_default_style_offline.json"))
+                    {
+                        using (var reader = new StreamReader(stream))
+                        {
+                            var result = await reader.ReadToEndAsync();
+                            MapView.SetStyleJson(result);
+                        }
+                    }*/
                     MapView.SetStyleUrl("https://raw.githubusercontent.com/antaresnav/antares-maps-sharp/develop/styles/basic_default_style_offline.json");
                     break;
+                case "Custom":
+                    string text = await InputTextDialogAsync("Style URL");
+                    MapView.SetStyleUrl(text, async (url,ex) =>
+                    {
+                        if(ex != null)
+                        {
+                            var dialog = new MessageDialog(ex.Message, "Error");
+                            await dialog.ShowAsync();
+                        }
+                    });
+                    break;
             }
+        }
+
+        private async Task<string> InputTextDialogAsync(string title)
+        {
+            TextBox inputTextBox = new TextBox();
+            inputTextBox.AcceptsReturn = false;
+            inputTextBox.Height = 32;
+            ContentDialog dialog = new ContentDialog();
+            dialog.Content = inputTextBox;
+            dialog.Title = title;
+            dialog.IsSecondaryButtonEnabled = true;
+            dialog.PrimaryButtonText = "Ok";
+            dialog.SecondaryButtonText = "Cancel";
+            if (await dialog.ShowAsync() == ContentDialogResult.Primary)
+                return inputTextBox.Text;
+            else
+                return "";
+        }
+
+        private void ReloadButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            MapView.ReloadStyleUrl();
         }
     }
 }
